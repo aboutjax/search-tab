5230ec576e39eaa37157a5e384a6d80295ea6fc0
Che Wei (Jacky) Lee <chewei.jacky.lee@gmail.com>
2025-08-20 11:52:29 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen bg-black text-white flex items-center justify-center">
+      <div className="text-xl">Hello World</div>
+    </div>
+  );
+}
+
+export default App;
